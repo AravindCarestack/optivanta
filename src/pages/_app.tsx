40daca58d144bf7c43e619d54382e64452d73eec
Aravind_Cp <aravindcp@carestack.com>
2025-01-29 12:17:27 +0530

    unwanted fonts removal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '~/styles/global.css'
+
 import type { AppProps } from 'next/app'
-import { IBM_Plex_Mono, Inter, PT_Serif, } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import { lazy } from 'react'
 
 export interface SharedPageProps {
@@ -10,31 +11,9 @@ export interface SharedPageProps {
 
 const PreviewProvider = lazy(() => import('~/components/PreviewProvider'))
 
-const mono = IBM_Plex_Mono({
-  variable: '--font-family-mono',
-  subsets: ['latin'],
-  weight: ['500', '700'],
-})
-
 const sans = Inter({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-})
-
-// const poppins = Poppins({
-//   subsets: ['latin'],
-//   display: 'swap',
-//   variable: '--font-poppins',
-//   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
-// });
-
-
-
-const serif = PT_Serif({
-  variable: '--font-family-serif',
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  weight: ['400', '700'],
 })
 
 export default function App({
@@ -46,13 +25,10 @@ export default function App({
     <>
       {draftMode ? (
         <PreviewProvider token={token}>
-          <Component {...pageProps} className={sans.className}/>
+          <Component {...pageProps} className={sans.className} />
         </PreviewProvider>
       ) : (
-
-        <Component {...pageProps} className={sans.className}/>
-
-    
+        <Component {...pageProps} className={sans.className} />
       )}
     </>
   )
